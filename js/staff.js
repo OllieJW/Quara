8fa08de38ebfa1e84decd5ae59d0d5d1d0f5966e
OllieJW <71931263+OllieJW@users.noreply.github.com>
2021-10-28 16:02:01 +0100

    Delete staff.js

--- js/staff.js
+++
@@ -1,9 +0,0 @@
-function staff(name, uuid, rank) {
-    let staffTemplate = $("#staff-template").html()
-        .replaceAll("{{ name }}", name)
-        .replaceAll("{{ rank }}", rank)
-		.replaceAll("{{ uuid }}", uuid)
-    $("#staff").append(staffTemplate)
-    
-    console.log("\n\n\nADDED STAFF ITEMS\n\n\n")
-}
