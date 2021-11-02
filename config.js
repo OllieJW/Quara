c6488ebf63f433e27bbef5dc318ccf6e1bfe315b
OllieJW <71931263+OllieJW@users.noreply.github.com>
2021-11-02 16:49:47 +0000

    1.2.2

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ staff("OJWMine", "86fa328f-a99a-4a17-8250-5d333dd327fe", "Owner");
 staff("OJWMine", "86fa328f-a99a-4a17-8250-5d333dd327fe", "Developer");
 staff("OJWMine", "86fa328f-a99a-4a17-8250-5d333dd327fe", "Admin");
 
-vote("Plant-Minecraft", "https://i.postimg.cc/9fmyrRkJ/Transparent-Planet-Minecraft-Logo.png", "https://planetminecraft.com");
+vote("Planet-Minecraft", "https://i.postimg.cc/9fmyrRkJ/Transparent-Planet-Minecraft-Logo.png", "https://planetminecraft.com");
 vote("Minecraft Servers Org", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://minecraftservers.org/");
 vote("Minecraft MP", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://minecraft-mp.com/");
 vote("Top Minecraft Servers", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://topminecraftservers.org/");
